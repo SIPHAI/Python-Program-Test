f563371d0f62b9c8c5ed0c6ee6bc827d7f397fcb
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-08-15 14:56:17 +0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,7 +2,8 @@
 #for if this code does not works I recommand you to install pytube first
 # So you can go directly on Powershell and type: pip install pytube
 from pytube import YouTube
-video_url = "https://www.youtube.com/watch?v=gdNW548KFVE "
+#Enter youtube video's Url that you want to download
+video_url = "Replace Url here "
 video = YouTube(video_url)
 print("Video title:",video.title)
 video = video.streams.get_highest_resolution()
