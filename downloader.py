450ce2cdc9ee67bdf3614d5ec88a988867f9aeb0
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-08-15 14:52:19 +0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,3 +1,6 @@
+
+#for if this code does not works I recommand you to install pytube first
+# So you can go directly on Powershell and type: pip install pytube
 from pytube import YouTube
 video_url = "https://www.youtube.com/watch?v=gdNW548KFVE "
 video = YouTube(video_url)
